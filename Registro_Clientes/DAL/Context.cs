79347aa0ab31f64eecc537f3e9d0fd8c67829787
Vladimir-gs <pedro_guzman1@ucne.edu.do>
2023-09-07 16:47:45 -0400

    Agregando modelo

---
+++ Registro_Clientes/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro_Clientes.Models;
+
+namespace Registro_Clientes.DAL
+{
+    public class Context : DbContext
+    {
+        public DbSet<Clientes> Clientes { get; set; }
+
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+    }
+}
