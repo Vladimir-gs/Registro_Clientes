03dad1fdb486435bfc930f1af010fa4a5b2eb140
Vladimir-gs <pedro_guzman1@ucne.edu.do>
2023-09-07 16:45:35 -0400

    Agregando modelo

---
+++ Registro_Clientes/Models/Clientes.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Registro_Clientes.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir el nombre del cliente")]
+        public string? Nombre { get; set; }
+
+        public string? Telefono { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir la cédula del cliente")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "Debe estar en un rango permitido")]
+        public string? Cedula { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir el RNC del cliente")]
+        [StringLength(9, MinimumLength = 9, ErrorMessage = "Debe estar en un rango permitido")]
+        public string? Rnc { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Direccion { get; set; }
+    }
+}
